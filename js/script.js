af621ce3716280dc13714cd9fcf9b3957381b69f
Vladislav Surin <vlad_surin@mail.ru>
2022-08-08 12:23:17 +0300

    added scripis for burger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-'use strict';
+"use strict";
+
+const burgerBtn = document.querySelector(".burger");
+const navMenu = document.querySelector(".nav");
+const navMenuLinks = navMenu.querySelectorAll("nav__link");
+const bodyEl = document.querySelector(".body");
+
+burgerBtn.addEventListener("click", () => {
+  burgerBtn.classList.toggle("burger--active");
+  navMenu.classList.toggle("nav--active");
+  bodyEl.classList.toggle("stop-scroll");
+
+  if (burgerBtn.classList.contains("burger--active")) {
+    burgerBtn.ariaLabel = "закрыть меню навигации";
+  } else {
+    burgerBtn.ariaLabel = "открыть меню навигации";
+  }
+});
+
+navMenu.addEventListener("click", (event) => {
+  if (!event.target.classList.contains("nav__link")) {
+    return;
+  }
+
+  burgerBtn.classList.remove("burger--active");
+  navMenu.classList.remove("nav--active");
+  bodyEl.classList.remove("stop-scroll");
+
+  if (burgerBtn.classList.contains("burger--active")) {
+    burgerBtn.ariaLabel = "закрыть меню навигации";
+  } else {
+    burgerBtn.ariaLabel = "открыть меню навигации";
+  }
+});
